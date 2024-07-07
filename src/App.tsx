a5dc7ed11fc958350e0aad9dd774b22d59141b7b
Athira Mohan <athiramohan@athiras-mbp.home>
2024-07-07 13:19:59 -0400

    Added folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Articles from './components/Articles';
+import AddArticle from './components/AddArticle';
 import './App.css';
+import { FormData } from './types';
 
-function App() {
+const App = () => {
+
+  const [open, setOpen] = useState<boolean>(false);
+  const [articles, setArticles] = useState<FormData[]>([]);
+
+  const addArticle = () => {
+    setOpen(true);
+  }
+
+  const handleArticleAdded = (article: FormData) => {
+    setArticles([
+      ...articles,
+      article
+    ]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
       </header>
+      <div>
+        <button onClick={addArticle}>Add article</button>
+        <AddArticle open={open} articleAdded={handleArticleAdded} />
+      </div>
+      <Articles articles={articles} />
     </div>
   );
 }
