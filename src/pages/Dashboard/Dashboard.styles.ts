1205dcac5a42a07448644d30cdceb48aea06a0c9
Athira Mohan <athiramohan@athiras-mbp.home>
2024-07-10 13:16:33 -0400

    Added responsive changes

--- src/pages/Dashboard/Dashboard.styles.ts
+++ src/pages/Dashboard/Dashboard.styles.ts
@@ -1,17 +1,23 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 export const ButtonWrap = styled.div`
-    display: flex;
-    justify-content: flex-end;
-`
+  display: flex;
+  justify-content: flex-end;
+`;
 
 export const DashboardWrap = styled.div`
-    display: flex;
-    flex-direction: row-reverse;
-    justify-content: space-between;
-    align-items: flex-start;
-    ul {
-        flex-basis: 80%;
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+  align-items: flex-start;
+  ul {
+    flex-basis: 80%;
+    @media (max-width: 768px) {
+      padding: 0;
     }
-`
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
